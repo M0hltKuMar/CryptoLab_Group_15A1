7ceb81811a2de5486754442398a6f2f53838ca24
M0hltKuMar <2024ucp1148@mnit.ac.in>
2026-08-14 15:46:02 +0530

    Update drone2.h

--- secure_application/src/drone2.h
+++ secure_application/src/drone2.h
@@ -1,1 +1,35 @@
+#ifndef DRONE2_H
+#define DRONE2_H
 
+#define MAX_WAYPOINTS 20
+
+typedef struct
+{
+    float latitude;
+    float longitude;
+} Waypoint;
+
+typedef struct
+{
+    int loggedIn;
+    char username[50];
+
+    Waypoint waypoints[MAX_WAYPOINTS];
+    int waypointCount;
+
+    int missionActive;
+} DroneSystem;
+
+void initializeSystem(DroneSystem *system);
+
+int login(DroneSystem *system);
+
+void uploadWaypoints(DroneSystem *system);
+
+void executeMission(DroneSystem *system);
+
+void displayTelemetry(DroneSystem *system);
+
+void viewLogs(void);
+
+#endif
