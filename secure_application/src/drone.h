a0e0512be3bd9b0a47a0fab2655d4ff9b2d9f626
Aryan Aman <cuballair@gmail.com>
2026-08-14 14:55:44 +0530

    Add files via upload
    header files for global access

--- secure_application/src/drone.h
+++ secure_application/src/drone.h
@@ -1,1 +1,24 @@
+#ifndef DRONE_H
+#define DRONE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Dynamic Global State
+extern int isAuthenticated;
+extern char currentWaypoint[32];
+extern char operatorName[32];
+extern int batteryLevel;
+extern int altitudeMeters;
+extern int isFlying;
+
+// Function Declarations
+void login(void);
+void uploadWaypoint(void);
+void executeMission(void);
+void displayTelemetry(void);
+void storeLog(void);
+
+#endif
